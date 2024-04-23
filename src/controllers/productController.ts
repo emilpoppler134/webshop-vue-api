d4bebcee25ebfb13cc2ca25ff42bfdea128bfce3
emilpoppler134 <emil@50archive.com>
2024-04-23 16:55:00 +0200

    make product controller return errors and validResponse with data

--- src/controllers/productController.ts
+++ src/controllers/productController.ts
@@ -1,40 +1,44 @@
+import { Request, Response } from "express";
 import { Types } from "mongoose";
+import { ICollection } from "../models/Collection.js";
+import { IProduct, Product } from "../models/Product.js";
+import { IStock } from "../models/Stock.js";
+import { ErrorCode, SuccessCode } from "../types/StatusCode.js";
+import { ErrorResponse, sendValidResponse } from "../utils/sendResponse.js";
 
-import { Product } from "../models/Product.js";
-
-import type { Request, Response } from "express";
-import { ErrorCode } from "../types/StatusCode.js";
-import { ErrorResponse } from "../utils/sendResponse.js";
+type ExtendedProduct = Omit<IProduct, "stock" | "collections"> & {
+  stock: Array<IStock>;
+  collections: Array<ICollection>;
+};
 
 async function list(req: Request, res: Response) {
-  try {
-    const products = await Product.find()
-      .populate({ path: "stock", model: "Stock" })
-      .populate({ path: "collections", model: "Collection" });
+  const products: Array<ExtendedProduct> = await Product.find()
+    .populate({ path: "stock", model: "Stock" })
+    .populate({ path: "collections", model: "Collection" });
 
-    res.json(products);
-  } catch {
-    throw new ErrorResponse(ErrorCode.SERVER_ERROR, "Something went wrong.");
-  }
+  return sendValidResponse<Array<ExtendedProduct>>(
+    res,
+    SuccessCode.OK,
+    products,
+  );
 }
 
 async function find(req: Request, res: Response) {
-  try {
-    const id = req.params.id;
+  const id = req.params.id;
 
-    if (id === undefined || !Types.ObjectId.isValid(id)) {
-      res.json({ status: "ERROR", data: "Not a valid ID" });
-      return;
-    }
-
-    const products = await Product.findById(id)
-      .populate({ path: "stock", model: "Stock" })
-      .populate({ path: "collections", model: "Collection" });
-
-    res.json(products);
-  } catch {
-    throw new ErrorResponse(ErrorCode.SERVER_ERROR, "Something went wrong.");
+  if (id === undefined || !Types.ObjectId.isValid(id)) {
+    throw new ErrorResponse(ErrorCode.BAD_REQUEST, "Not a valid ID.");
   }
+
+  const product: ExtendedProduct | null = await Product.findById(id)
+    .populate({ path: "stock", model: "Stock" })
+    .populate({ path: "collections", model: "Collection" });
+
+  if (product === null) {
+    throw new ErrorResponse(ErrorCode.NO_RESULT, "No product with that id.");
+  }
+
+  return sendValidResponse<ExtendedProduct>(res, SuccessCode.OK, product);
 }
 
 export default { list, find };
